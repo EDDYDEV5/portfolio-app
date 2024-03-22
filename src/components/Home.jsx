b23915aa80eafad4e2291215d054df6d66e26ce8
EDDYDEV5 <cletusobosi5@gmail.com>
2024-03-22 10:07:03 +0100

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import heroimage from "../assets/heroimage.png";
-import { FiDownload } from "react-icons/fi"
+import { FiDownload } from "react-icons/fi";
 
 const Home = () => {
   const links = [
@@ -14,23 +14,33 @@ const Home = () => {
 
   return (
     <>
-      <div name="home" className="h-fit w-full bg-gradient-to-bl from-slate-50 to-slate-400 pt-20">
-        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-screen px-4 md:flex-row">
+      <div
+        name="home"
+        className="h-fit w-full  pt-20 bg-gradient-to-tr bg-green-800 to-purple-400 "
+      >
+        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-screen text-yellow-500 px-4 md:flex-row">
           <div className=" flex flex-col justify-center h-full ">
-            <h1 className=" mt-2 text-5xl md:text-7xl font-tektur text-slate-600 capitalize">
+            <h1 className=" mt-2 text-5xl md:text-7xl font-playfair font-bold font  capitalize">
               hi, <br /> i'm cletus obosi
             </h1>
-            <p className=" capitalize tracking-wide py-4 max-w-md text-slate-600 text-xl font-semibold">
-              a dedicated and result oriented <br />frontend developer creating great
-              user experience and responsive webistes.
+            <p className=" capitalize tracking-wide py-4 max-w-md  text-xl font-inter ">
+              a dedicated and result oriented <br />
+              frontend developer creating great user experience and responsive
+              webistes.
             </p>
             <div>
-              <div className=" cursor-pointer text-xl capitalize flex  rounded-md bg-red-950 text-white justify-center items-center px-5 mt-2 w-40 bg-gradient-to-r from-amber-300 to-amber-500 hover:text-slate-600 py-3 delay-75 font-tektur">
+              <div className=" cursor-pointer text-xl capitalize flex  rounded-md text-green-800 justify-center items-center px-5 mt-2 w-40  hover:text- hover:bg-slate-100 py-3 delay-75 font-tektur bg-yellow-500">
                 <ul>
                   {links.map(({ id, child, href, download }) => (
                     <li key={id}>
-                      <a href={href} download={download} target="_blank"className=" flex flex-row">
-                        {child}<FiDownload size={20} className=' ml-2 mt-1' />
+                      <a
+                        href={href}
+                        download={download}
+                        target="_blank"
+                        className=" flex flex-row"
+                      >
+                        {child}
+                        <FiDownload size={20} className=" ml-2 mt-1" />
                       </a>
                     </li>
                   ))}
@@ -39,11 +49,11 @@ const Home = () => {
             </div>
           </div>
 
-          <div>
+          <div className=" pb-3">
             <img
               src={heroimage}
               alt="my hero profile"
-              className=" rounded-full mx-auto w-2/3 md:w-full border-4 border-amber-400"
+              className=" rounded-full mx-auto w-2/3 md:w-full border-4 border-yellow-500"
             />
           </div>
         </div>
